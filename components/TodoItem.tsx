870d635c36ede3011d54e00e4ff662fcec2825fb
Aditya Erlangga Wibowo <adityaerlangga2003@gmail.com>
2025-12-11 01:10:01 +0700

    Add year to date displays in todo cards (due date and created at)

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -229,7 +229,7 @@ export default function TodoItem({ todo, onEdit, onDelete, onToggleProgress }: T
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
               </svg>
             )}
-            <span className="truncate">Due: {format(new Date(todo.dueDate), 'MMM dd')}</span>
+            <span className="truncate">Due: {format(new Date(todo.dueDate), 'MMM dd, yyyy')}</span>
           </div>
         )}
       </div>
@@ -241,7 +241,7 @@ export default function TodoItem({ todo, onEdit, onDelete, onToggleProgress }: T
             <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span className="truncate">{format(new Date(todo.createdAt), 'MMM dd')}</span>
+            <span className="truncate">{format(new Date(todo.createdAt), 'MMM dd, yyyy')}</span>
           </div>
         </div>
         <div className="flex gap-2">
